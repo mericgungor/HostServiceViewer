280b465c4179546c7ea0abcb638a9ee504a2405a
Meriç GÜNGÖR <mericgungor@gmail.com>
2021-12-29 16:46:40 +0300

    web api tamamlandı

---
+++ _Project/HostServiceViewer/Business/NetworkManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace Business
+{
+    public class NetworkManager
+    {
+        public bool Ping(string ip)
+        {
+            var ping = new Ping();
+            var reply = ping.Send(ip, 1000); // 1 minute time out (in ms)
+            if (reply.Status == IPStatus.Success)
+                return true;
+            else
+                return false;
+        }
+
+        public bool Telnet(string ip, int port)
+        {
+
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.ReceiveTimeout = 1;
+                socket.SendTimeout = 1;
+
+                IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                //------------------- 1. Yol -------------------------------
+
+                //try
+                //{
+                //    socket.Connect(remoteEndPoint);
+
+                //    result = true;
+                //}
+                //catch
+                //{
+                //    result = false;
+                //}
+                //------------------- 2. Yol -------------------------------
+                try
+                {
+                    //socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, false);
+
+
+                    IAsyncResult sResult = socket.BeginConnect(ip, port, null, null);
+                    bool success = sResult.AsyncWaitHandle.WaitOne(1000, true);
+
+                    return socket.Connected;
+                }
+                catch
+                {
+                    return false;
+                }
+                finally
+                {
+                    if (null != socket)
+                        socket.Close();
+                }
+            }
+        }
+        public bool Page(string url)
+        {
+
+            WebRequest request = WebRequest.Create(url);
+            using (WebResponse response = request.GetResponse())
+            {
+                var statusCode = ((HttpWebResponse)response).StatusCode;
+                if (statusCode == HttpStatusCode.OK)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ _Project/HostServiceViewer/WebApi/Controllers/ApiController.cs
@@ -0,0 +1,96 @@
+﻿using Business;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private NetworkManager _networkManager;
+
+        public ApiController()
+        {
+            _networkManager = new NetworkManager();
+        }
+
+
+        [HttpGet("ping")]
+        public IActionResult Ping(string ip)
+        {
+            if (string.IsNullOrEmpty(ip) || !ip.Contains(".") || ip.Split('.').Length != 4)
+                return FalseResult();
+
+
+
+            var result = _networkManager.Ping(ip);
+            if (result)
+            {
+                return TrueResult();
+            }
+            else
+            {
+                return FalseResult();
+            }
+
+        }
+
+        [HttpGet("telnet")]
+        public IActionResult Telnet(string ip, int port)
+        {
+            if (string.IsNullOrEmpty(ip) || !ip.Contains(".") || ip.Split('.').Length != 4 || port==0)
+                return FalseResult();
+
+
+
+            var result = _networkManager.Telnet(ip,port);
+            if (result)
+            {
+                return TrueResult();
+            }
+            else
+            {
+                return FalseResult();
+            }
+        }
+        [HttpGet("page")]
+        public IActionResult Page(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return FalseResult();
+
+
+
+            var result = _networkManager.Page(url);
+            if (result)
+            {
+                return TrueResult();
+            }
+            else
+            {
+                return FalseResult();
+            }
+        }
+
+
+        public ObjectResult TrueResult(object obj = null)
+        {
+            if (obj == null)
+                obj = true;
+
+            return Ok(obj);//başarılı ise            
+        }
+        public ObjectResult FalseResult(object obj = null)
+        {
+            if (obj == null)
+                obj = false;
+
+            return StatusCode(Microsoft.AspNetCore.Http.StatusCodes.Status408RequestTimeout, obj);//başarısız ise            
+        }
+    }
+}
